ec514cb84901dfa7182819d7069269aa6c6fd5a4
tony <tony@yiqizu.com>
2022-06-14 07:01:10 +0800

    默认不安装mongol

--- mongo/docker-entrypoint-initdb.d/mongo-init.sh
+++ mongo/docker-entrypoint-initdb.d/mongo-init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Creating mongo users..."
-mongo --authenticationDatabase admin --host localhost -u root -p 'mySecret' --eval "db.createUser({user: 'restydock', pwd: 'secret', roles: [{role: 'readWrite', db: 'test'}]}); db.createUser({user: 'admin', pwd: 'mySecret', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
+# 打开下面注释即可在生成容器时创建用户，使用前先修改成自己的账号和密码。如未生效请删掉宿主机上 ~/.resty/data 下面的 mongo 和 mongo_config 目录然后再生成容器即可正常执行
+# mongo --authenticationDatabase admin --host localhost -u root -p 'mySecret' --eval "db.createUser({user: 'restydock', pwd: 'mySecret', roles: [{role: 'readWrite', db: 'test'}]}); db.createUser({user: 'admin', pwd: 'mySecret', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});"
 echo "Mongo users created."
